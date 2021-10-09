5715c086b4fe1817c2e4592b5b6b03ef69ef1e0c
Marco Bispo <46981041+MarcoBispo97@users.noreply.github.com>
2021-10-09 13:45:04 -0300

    Add files via upload

---
+++ Join.py
@@ -0,0 +1,9 @@
+# I am learning Python and improve my english too, future python developer
+
+future = []
+
+future = ["I","will","be","a","python","developer"]
+
+planFuture = (" ".join(future))
+
+print(planFuture)
